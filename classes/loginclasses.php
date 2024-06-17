9b8061d8d2df4fd0e0f5ebe029557dc05eff4b26
AsuraMzn <ribubaucha@gmail.com>
2024-06-17 22:33:56 +0545

    Fix the password Decrypt error
    Now able to decrypt the password and check if passwords match

--- classes/loginclasses.php
+++ classes/loginclasses.php
@@ -27,26 +27,50 @@ class Login extends Dbh {
         $key = hash_pbkdf2("sha256", $pwd, $salt, $iterations, $keyLength, true);
 
         // Check if password is correct
-        $stmt = $this->connect()->prepare('SELECT * FROM users WHERE (users_name = ? OR users_email = ?) AND users_pwd = AES_ENCRYPT(?,?);');
-
-        if(!$stmt->execute(array($uname, $uname, $pwd, $key))) {
+        $stmt = $this->connect()->prepare('SELECT AES_DECRYPT(users_pwd, ?) AS decrypted_password FROM users WHERE users_name = ? OR users_email = ?');
+        if(!$stmt->execute(array($key, $uname, $uname))) {
             $stmt = null;
             header("location: ../login.php?error=stmtfailed");
             exit();
         }
-
+    
         if ($stmt->rowCount() == 0) {
             $stmt = null;
-            header("location: ../login.php?error=incorrectpassword");
+            header("location: ../login.php?error=usernotfound");
             exit();
         }
-
-        $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        session_start();
-        $_SESSION["userid"] = $user[0]["users_id"];
-        $_SESSION["username"] = $user[0]["users_name"];
         
+        $decryptPwd = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+        $checkPwd = ($pwd == $decryptPwd[0]['decrypted_password']) ? true : false;
+            
+        if($checkPwd == false) {
+            $stmt = null;
+            header("location: ../login.php?error=wrongpassword");
+            exit();
+        }
+        elseif($checkPwd == true) {
+            $stmt = $this->connect()->prepare('SELECT * FROM users WHERE (users_name = ? OR users_email = ?) AND users_pwd = AES_ENCRYPT(?,?);');
+
+            if(!$stmt->execute(array($uname, $uname, $pwd, $key))) {
+                $stmt = null;
+                header("location: ../login.php?error=stmtfailed");
+                exit();
+            }
+
+            if ($stmt->rowCount() == 0) {
+                $stmt = null;
+                header("location: ../login.php?error=usernotfound");
+                exit();
+            }
+
+            $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            session_start();
+            $_SESSION["userid"] = $user[0]["users_id"];
+            $_SESSION["username"] = $user[0]["users_name"];
+        }
+
         $stmt= null;
     }
 }
